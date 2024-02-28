0b21dbdec2433673a77b04f6722f0441cd7b8fdc
punSavitri <punshakshi@gmail.com>
2024-02-28 16:41:34 +0000

    update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ function updateCityTime() {
   let chicagoTimeZone = moment().tz("America/Chicago");
   //console.log(chicagoTimeZone);
   chicagoDateElement.innerHTML = chicagoTimeZone.format("MMMM Do YYYY");
-  chicagoTimeElement.innerHTML = chicagoTimeZone.format("HH:mm:ss[<small>]A[</small>]");
+  chicagoTimeElement.innerHTML = chicagoTimeZone.format("HH:mm:ss:SS[<small>]A[</small>]");
 
   //hongkong city
   let hongkongCityElement = document.querySelector("#hongkongCity");
@@ -17,8 +17,8 @@ function updateCityTime() {
   let hongkongTimeZone = moment().tz("Asia/Hong_Kong");
   //console.log(hongkongTimeZone);
   hongkongDateElement.innerHTML = hongkongTimeZone.format("MMMM Do YYYY");
-  hongkongTimeElement.innerHTML = hongkongTimeZone.format("HH:mm:ss[<small>]A[</small>]");
+  hongkongTimeElement.innerHTML = hongkongTimeZone.format("HH:mm:ss:SS[<small>]A[</small>]");
 
 }
 updateCityTime();
-setInterval(updateCityTime, 1000);
+setInterval(updateCityTime, 1);
